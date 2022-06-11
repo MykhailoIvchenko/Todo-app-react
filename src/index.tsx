6593e3d07f3c67f42e572d69f8ea3b858fc75546
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-06-11 22:22:47 +0300

    Clone todo app

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+
+import './styles/index.scss';
+import './styles/todo-list.scss';
+import './styles/filters.scss';
+
+import TodoApp from './TodoApp';
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('root'),
+);
